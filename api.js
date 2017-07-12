d6f2b2eda853dcb815c8f79e204a024eebb08247
John Jastrow <jjastrow>
2017-07-12 17:21:43 -0500

    updates for heroku

--- api.js
+++ api.js
@@ -1,11 +1,17 @@
 const express = require("express");
 const app = express();
 
+app.get("/", (req,res) => {
+    res.send( "<h3>This is an API. Try: /api/hello</h3>" );
+});
+
 app.get("/api/hello", (req,res) => {
     res.json( {"hello": "world"} );
 });
 
 //////////
-app.listen(3000);
+app.listen(3000, function(){
+    console.log("Started. Listening on PORT 3000 (example: http://localhost:3000/api/hello)");
+});
 
 module.exports = app;
