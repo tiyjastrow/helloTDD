81be7ea3b8bf2101c970c278d0c604d53babfbd8
John Jastrow <jjastrow>
2017-07-12 17:27:32 -0500

    fix port ?

--- api.js
+++ api.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+var port = process.env.PORT || 3000;
 
 app.get("/", (req,res) => {
     res.send( "<h3>This is an API. Try: /api/hello</h3>" );
@@ -10,8 +11,8 @@ app.get("/api/hello", (req,res) => {
 });
 
 //////////
-app.listen(3000, function(){
-    console.log("Started. Listening on PORT 3000 (example: http://localhost:3000/api/hello)");
+app.listen(port, function(){
+    console.log(`Started. Listening on PORT ${port} (example: http://localhost:3000/api/hello)`);
 });
 
 module.exports = app;
